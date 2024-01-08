4c81966ad8ecc5a07b189f551d12d82aa0357ba8
sc2bat <izar_dero@naver.com>
2024-01-08 15:25:51 +0900

    update TextField border

--- lib/screen/main_page.dart
+++ lib/screen/main_page.dart
@@ -11,20 +11,21 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('도서관 일정 검색'),
+      appBar: AppBar(
+        title: const Text('도서관 일정 검색'),
       ),
-      body: const Column(
-        children: [
-          TextField( decoration: InputDecoration(
-            labelText: 'Enter text',
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.blue, width: 2.0),
-            ),
-          ),
-
-
-          )
-        ],
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter text',
+                border: OutlineInputBorder(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
